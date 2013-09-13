9fc77ed80c6aea7fcff4d880295fd29968ef25eb
Jeff Hann <jeff.hann@teamspace.ca>
2013-09-13 11:17:30 -0300

    updating for brew, deleting old screenshots

---
+++ archey.rb
@@ -0,0 +1,14 @@
+require 'formula'
+
+# Documentation: https://github.com/mxcl/homebrew/wiki/Formula-Cookbook
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+
+class Archey < Formula
+  homepage 'http://obihann.github.io/archey-osx/'
+  #url 'https://github.com/obihann/archey-osx/archive/1.0.tar.gz'
+  url 'https://github.com/obihann/archey-osx.git'
+  sha1 '5a470715fadb38b3d78bb296e3c881af53df08a0'
+
+  def install
+  end
+end
