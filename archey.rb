4e921ab218caf972054df9b1042ccdf2e3e3a7f1
Jeff Hann <jeff.hann@teamspace.ca>
2013-09-13 11:34:39 -0300

    fixing a bug

--- archey.rb
+++ archey.rb
@@ -1,14 +1,12 @@
 require 'formula'
 
-# Documentation: https://github.com/mxcl/homebrew/wiki/Formula-Cookbook
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
-
 class Archey < Formula
   homepage 'http://obihann.github.io/archey-osx/'
-  #url 'https://github.com/obihann/archey-osx/archive/1.0.tar.gz'
   url 'https://github.com/obihann/archey-osx.git'
   sha1 '5a470715fadb38b3d78bb296e3c881af53df08a0'
+  version "1.0"
 
   def install
+    system "cp bin/archey #{prefix}/"
   end
 end
