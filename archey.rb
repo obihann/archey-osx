7c61ae99eb6f212eea16ca196bbc485614818983
Jeff Hann <jeff.hann@teamspace.ca>
2013-09-13 11:40:49 -0300

    updating brew formula

--- archey.rb
+++ archey.rb
@@ -2,11 +2,12 @@ require 'formula'
 
 class Archey < Formula
   homepage 'http://obihann.github.io/archey-osx/'
-  url 'https://github.com/obihann/archey-osx.git'
-  sha1 '5a470715fadb38b3d78bb296e3c881af53df08a0'
+  url 'https://github.com/obihann/archey-osx/archive/1.1.tar.gz'
+  sha1 '52da445c38c08415caa8ba93d28bbfe7b4ac1003'
   version "1.0"
 
   def install
-    system "cp bin/archey #{prefix}/"
+    system "mkdir #{bin}"
+    system "cp bin/archey #{bin}/"
   end
 end
